3f042f19fcfdb4f013b67489143a39124846d954
284772894 <284772894@qq.com>
2020-02-13 18:49:58 +0800

    bili视频下载

---
+++ bilibili.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding=utf-8 -*-
+from concurrent.futures import ThreadPoolExecutor
+
+__author__ = 'shikun'
+__CreateAt__ = '2020/2/13-10:19'
+import os
+
+
+def download_pool(play_list):
+    """
+    线程池下载视频
+    :param play_list: 下载视频列表
+    :return:
+    """
+    with ThreadPoolExecutor(len(play_list)) as pool:
+        pool.map(download, play_list)
+
+
+def get_play_list():
+    url = "https://www.bilibili.com/video/av14800010?p="
+    data = []
+    for i in range(1, 25):
+        data.append(url + str(i))
+    return data
+
+
+def download(url):
+    """
+    下载视频
+    :param url:
+    :return:
+    """
+    path = r"E:\bilibili\造物小百科.第三季"
+    command = "you-get %s -o  %s" % (url, path)
+    print(command)
+    resp = os.system(command)
+    print(resp)
+
+
+if __name__ == "__main__":
+    pl_list = get_play_list()
+    download_pool(pl_list)
